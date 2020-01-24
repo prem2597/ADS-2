2426d19826091b6b16be3062e7253c62b9d58a02
prem2597 <prembammidi25@msitprogram.net>
2020-01-24 09:44:16 +0530

    Git Commit

--- Tex.java
+++ Tex.java
@@ -5,63 +5,63 @@ import java.math.*;
 import java.util.regex.*;
 import java.util.Scanner;
 public class Tex {
-    // static String newString = "";
-    // public static String insertString(String string, String stringToBeInserted, int index) {
-    //     newString = new String();
-    //     for (int i = 0; i < string.length(); i++) {
-    //         if (i == index) {
-    //             // newString.replace
-    //             // newString = newString.substring(i,);
-    //             newString += stringToBeInserted;
-    //         } else {
-    //             newString += string.charAt(i);
-    //         }
-    //         // System.out.println(newString);  
-    //     }
-    //     // System.out.println(newString);
-    //     return newString;
-    // }
+    static String newString = "";
+    public static String insertString(String string, String stringToBeInserted, int index) {
+        newString = new String();
+        for (int i = 0; i < string.length(); i++) {
+            if (i == index) {
+                // newString.replace
+                // newString = newString.substring(i,);
+                newString += stringToBeInserted;
+            } else {
+                newString += string.charAt(i);
+            }
+            // System.out.println(newString);  
+        }
+        // System.out.println(newString);
+        return newString;
+    }
 
-    // public static void main(String[] args) {
-    //     // String newString = "";
-    //     /*
-    //      * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
-    //      * class should be named Solution.
-    //      */
-    //     Scanner sc = new Scanner(System.in);
-    //     // while(sc.hasNextLine()) {
-    //     // String x = sc.nextLine();
-    //     // for (int i = 0; i < x.length(); i++) {
-    //     // if (x.charAt(i) == '\"') {
+    public static void main(String[] args) {
+        // String newString = "";
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        // while(sc.hasNextLine()) {
+        // String x = sc.nextLine();
+        // for (int i = 0; i < x.length(); i++) {
+        // if (x.charAt(i) == '\"') {
 
-    //     // }
-    //     // }
-    //     // }
-    //     while (sc.hasNextLine()) {
-    //         String x = sc.nextLine();
-    //         StringBuilder string = new StringBuilder(x);
-    //         String m = x;
-    //         // int count = 0;
-    //         boolean h = true;
-    //         for (int i = 0; i <= x.length(); i++) {
-    //             // m = x;
-    //             if (m.charAt(i) == '\"') {
-    //                 // count = count + 1;
-    //                 h = !h;
-    //                 if (h == false) {
-    //                     // string.setCharAt(i,'`');
+        // }
+        // }
+        // }
+        while (sc.hasNextLine()) {
+            String x = sc.nextLine();
+            StringBuffer string = new StringBuffer(x);
+            String m = x;
+            // int count = 0;
+            boolean h = true;
+            for (int i = 0; i <= x.length(); i++) {
+                // m = x;
+                if (m.charAt(i) == '\"') {
+                    // count = count + 1;
+                    h = !h;
+                    if (h == false) {
+                        // string.setCharAt(i,'`');
                         
-    //                     m = insertString(m, "``", i);
+                        m = insertString(m, "``", i);
     
-    //                 } else if (h == true) {
-    //                     // string.setCharAt(i, '\"');
-    //                     m = insertString(m, "''", i);
-    //                 }
-    //             }
-    //         }
-    //         System.out.println(m);
-    //     }
-    // }
+                    } else if (h == true) {
+                        // string.setCharAt(i, '\"');
+                        m = insertString(m, "''", i);
+                    }
+                }
+            }
+            System.out.println(m);
+        }
+    }
     // public static void main(String[] args) {
     //     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     //     Scanner sc = new Scanner(System.in);
@@ -87,13 +87,13 @@ public class Tex {
     //     }
     // sc.close();
     // }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            String x = sc.nextLine();
-            for (int i = 0; i < x.length(); i++) {
-                System.out.println(x.charAt(i));
-            }
-        }
-    }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     while(sc.hasNextLine()) {
+    //         String x = sc.nextLine();
+    //         for (int i = 0; i < x.length(); i++) {
+    //             System.out.println(x.charAt(i));
+    //         }
+    //     }
+    // }
 }
